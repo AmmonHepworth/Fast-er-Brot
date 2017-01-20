71781adc2be61b0c75e633e1b300a76b500c2158
Ammon <ammonhepworth@outlook.com>
2017-01-19 18:29:33 -0700

    changed image size to be lower for pi

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
 	std::cout << "THIS PROGRAM ONLY OUTPUTS ONE IMAGE TO A FILE, IT ONLY GENERATES THE OTHERS" << std::endl;
 	std::cout << "Five image generations will be used for the calculations." << std::endl;
 
-	Mandelbrot2 m1("m2.ppm",1500,1500,(double)-2,(double)1,(double)1,(double)-1,1024, 4);
+	Mandelbrot2 m1("m2.ppm",512,512,(double)-2,(double)1,(double)1,(double)-1,1024, 4);
 	Timer mand1,mand2;
 
 	std::cout << std::endl << "Warming up the cache for accurate timings, please wait..." << std::endl;
@@ -16,7 +16,7 @@ int main(){
 	m1.generateParallel();
 	for(int i=0;i<5;++i)
 	{
-		std::cout << mand1.timeTask<Mandelbrot2, &Mandelbrot2::generateParallel>(new Mandelbrot2("m1.ppm", 1500, 1500,(double)-2,(double)1,(double)1,(double)-1, 1024, 4)) << std::endl;
+		std::cout << mand1.timeTask<Mandelbrot2, &Mandelbrot2::generateParallel>(new Mandelbrot2("m1.ppm", 512, 512,(double)-2,(double)1,(double)1,(double)-1, 1024, 4)) << std::endl;
 	}
 	std::cout << "Average: " << mand1.getAverageTime() << std::endl;
 	std::cout << "Std Dev: " << mand1.getStdDev() << std::endl;
@@ -24,7 +24,7 @@ int main(){
 	
 	for(int i=0;i<5;++i)
 	{
-		std::cout << mand2.timeTask<Mandelbrot2, &Mandelbrot2::generate>(new Mandelbrot2("m1.ppm", 1500, 1500,(double)-2,(double)1,(double)1,(double)-1, 1024, 4)) << std::endl;
+		std::cout << mand2.timeTask<Mandelbrot2, &Mandelbrot2::generate>(new Mandelbrot2("m1.ppm", 512, 512,(double)-2,(double)1,(double)1,(double)-1, 1024, 4)) << std::endl;
 	}
 	std::cout << "Average: " << mand2.getAverageTime() << std::endl;
 	std::cout << "Std Dev: " << mand2.getStdDev() << std::endl;
